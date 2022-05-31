46d73e7623503cc64b01a9074ee3a932e2ab11eb
sean <sr.webcode@gmail.com>
2022-05-31 08:10:23 +0800

    add lint to workflow/ update button jsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 const Button: React.FC = () => {
   const myFavNumber = 0
-  return <div>hi this is the button: {myFavNumber}</div>
+  return <div>hi this is the button updated: {myFavNumber}</div>
 }
 export default Button

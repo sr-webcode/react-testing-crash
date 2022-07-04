e8f27f2eac9c54b001a026b728292774ed75a6ab
sean <sr.webcode@gmail.com>
2022-07-04 21:38:40 +0800

    failing test example

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Home from './index';
 
-describe('it has wow text', () => {
-  it('finds wow textes', () => {
+describe('initial home page test', () => {
+  it('finds wow text', () => {
     render(
       <BrowserRouter>
         <Home />
       </BrowserRouter>
     );
-    const wowText = screen.getByText(/wow/);
+    const wowText = screen.getByText(/wowzer/);
     expect(wowText).toBeInTheDocument();
   });
 });
